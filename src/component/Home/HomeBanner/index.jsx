3497607ab8e3e12b79b5fa1f90ea35b6a63bc30e
Veeravel-S <veeravel.igt@gmail.com>
2025-09-06 13:23:07 +0530

    UI changes

--- src/component/Home/HomeBanner/index.jsx
+++ src/component/Home/HomeBanner/index.jsx
@@ -5,8 +5,8 @@ const HomeBanner = ({ data,statsData }) => {
     return (
         <div className="container-md-fluid d-flex justify-content-center" style={{ backgroundColor: '#f0fcff' }}>
             <div className={`${styles.container} d-flex justify-content-center align-items-end w-100`}>
-                <div className="w-100 d-flex gap-5 align-items-end flex-column flex-lg-row">
-                    <div className="d-flex w-100 justify-content-end pb-4 flex-column mb-4 px-4 px-md-0 mb-md-0">
+                <div className="w-100 d-flex gap-lg-5 align-items-end flex-column flex-lg-row">
+                    <div className="d-flex w-100 justify-content-end pb-lg-4 flex-column mb-lg-4 px-4 px-md-0 mb-md-0">
                         <h1 className={`fw-bold fs-1 ${styles.title}`}>
                             {data?.title}
                         </h1>
